f54a668206b4af03022e5006982b4fb966e71d48
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-07-10 14:57:02 -0700

    task

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+/**
+ * check_cycle - Entry point
+ * Description: check cycle
+ * @list: head
+ * Return: 1 or 0
+ */
+int check_cycle(listint_t *list)
+{
+	int passed = 0;
+	listint_t *current;
+	listint_t *head = list;
+
+	current = list;
+	while (current != NULL)
+	{
+		if (passed == 0 && current == head)
+			passed = 1;
+		else if (passed == 1 && current == head)
+			return (1);
+		current == current->next;
+	}
+	return (0);
+}
