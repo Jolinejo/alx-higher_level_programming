5cce94a92d679191a37cc69f0ff0cab7e35e831f
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-07-11 13:23:44 -0700

    task

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def pow(a, b):
-    if b == 0:
-        return (1)
-    return (a * pow(a, b - 1))
+    result = 1
+    if b > 0:
+        for i in range(0, b):
+            result = result * a
+    elif b < 0:
+        for i in range(b, 0):
+            result = result / a
+    return (result)
