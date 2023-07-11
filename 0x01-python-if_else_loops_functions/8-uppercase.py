27bbba4cbf6767850446dd408843b181c6b8a302
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-07-11 13:07:57 -0700

    task

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for let in str:
+        if ord(let) >= ord('a') and ord(let) <= ord('z'):
+            c = ord(let) - ord('a') + ord('A')
+        else:
+            c = let
+        print("{alpha}".format(alpha=c), end='')
+    print("")
