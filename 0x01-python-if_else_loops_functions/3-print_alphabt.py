9e18561dc90f08ccb3c0d63c485532bb7aaa74af
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-07-11 12:11:52 -0700

    task

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for ascii_val in range(ord('a'), ord('z') + 1):
-    if ascii_val != ord('c') and ascii_val != ord('q'):
+    if ascii_val != ord('e') and ascii_val != ord('q'):
         print("{alpha}".format(alpha=chr(ascii_val)), end='')
