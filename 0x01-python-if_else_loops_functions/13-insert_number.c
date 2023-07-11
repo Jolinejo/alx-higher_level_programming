bd0ba35162aae8a61fe3461b0cd6ff1b2b73e719
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-07-11 13:53:09 -0700

    task

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * insert_node - Entry point
+ * Description: insert sorted
+ * @head: head
+ * @number: n
+ * Return: nothing
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new = malloc(sizeof(listint_t));
+	listint_t *prev;
+	listint_t *curr = *head;
+
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
+	if (*head == NULL)
+	{
+		new->next = NULL;
+		return (new);
+	}
+	while (curr->n < number && curr->next != NULL)
+	{
+		prev = curr;
+		curr = curr->next;
+	}
+	if (curr->n < number)
+	{
+		curr->next = new;
+		new->next = NULL;
+	}
+	else
+	{
+		prev->next = new;
+		new->next = curr;
+	}
+	return (new);
+}
