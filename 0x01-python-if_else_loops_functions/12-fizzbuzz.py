9c29b43b084d1b28c3b7466b30fb203ccdbefb66
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-07-11 13:35:32 -0700

    task

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+def fizzbuzz():
+    for i in range (1, 101):
+        if i % 15 == 0:
+            print("{num} ".format(num="FizzBuzz"), end='')
+        elif i % 5 == 0:
+            print("{num} ".format(num="Buzz"), end='')
+        elif i % 3 == 0:
+            print("{num} ".format(num="Fizz"), end='')
+        else:
+            print("{num} ".format(num=i), end='')
+    print("")
