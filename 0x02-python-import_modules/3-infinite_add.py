24fba5de791dc6a8ec52ef7113d86f128464fdf8
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-07-13 07:48:33 -0700

    sum

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def main():
+    import sys
+    n = len(sys.argv)
+    summ = 0;
+    for i in range(1, n):
+        summ = summ + int(sys.argv[i])
+    print(summ)
+
+
+if __name__ == "__main__":
+    main()
