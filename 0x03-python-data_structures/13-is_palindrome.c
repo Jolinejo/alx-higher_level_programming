ebe332b9a941e7e76d17667710ec4804daad5c97
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-07-17 09:08:09 -0700

    task

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -3,6 +3,31 @@
 #include "lists.h"
 
 /**
+ * check - check the code for
+ * @head: head
+ * @len: len
+ * Return: Always 0.
+ */
+int check(const int len, listint_t **head)
+{
+	int numbers[len];
+	int i = 0;
+	listint_t *curr = *head;
+
+	for (i; i < len; i++)
+	{
+		numbers[i] = curr->n;
+		curr = curr->next;
+	}
+	for (i = 0; i < len / 2; i++)
+	{
+		if (numbers[i] != numbers[len - i - 1])
+			return (0);
+	}
+	return (1);
+}
+
+/**
  * is_palindrome - check the code for
  * @head: head
  * Return: Always 0.
@@ -26,15 +51,5 @@ int is_palindrome(listint_t **head)
 	len++;
 	if (nex->n != curr->n)
 		return (0);
-	for (i = 1; i < len / 2; i++)
-	{
-		curr = curr->next;
-		nex = curr;
-		for (j = i; j < len - i - 1; j++)
-			nex = nex->next;
-		if (nex->n != curr->n)
-			return (0);
-	}
-	return (1);
-
+	return (check(len, head));
 }
