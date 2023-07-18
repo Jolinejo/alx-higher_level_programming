48c44ee2ac4352b99b325f55609ac232b6c42c62
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-07-18 12:31:59 -0700

    task

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+def main():
+    import calculator_1 as calc
+    import sys
+    n = len(sys.argv)
+    if n != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    if not sys.argv[2] in "+*-/":
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    if sys.argv[2] == '+':
+        print("{a} {op} {b} = {re}".format(a=a, b=b, op=sys.argv[2], re=calc.add(a, b)))
+    elif sys.argv[2] == '-':
+        print("{a} {op} {b} = {re}".format(a=a, b=b, op=sys.argv[2], re=calc.sub(a, b)))
+    elif sys.argv[2] == '*':
+        print("{a} {op} {b} = {re}".format(a=a, b=b, op=sys.argv[2], re=calc.mul(a, b)))
+    if sys.argv[2] == '/':
+        print("{a} {op} {b} = {re}".format(a=a, b=b, op=sys.argv[2], re=calc.div(a, b)))
+
+
+if __name__ == "__main__":
+    main()
