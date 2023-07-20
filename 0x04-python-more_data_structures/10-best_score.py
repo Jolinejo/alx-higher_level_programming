71b385117ef7085efca353cfad2018c80f99f593
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-07-20 02:14:15 -0700

    task

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     maxn = 0
+    best_k = None
     if len(a_dictionary) == 0:
         return None
     for key in a_dictionary:
         if a_dictionary[key] > maxn:
             maxn = a_dictionary[key]
-    return (maxn)
+            best_k = key
+    return (best_k)
