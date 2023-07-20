ee1961961624fe8923a3c1736e0bae0e2af1e62e
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-07-20 02:26:42 -0700

    task

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    new_matrix = []
-    if my_list == []:
-        return new_matrix
-    new_matrix = my_list[:]
-    new_matrix = list(map(lambda x: x * number, new_matrix))
-    return new_matrix
+    return list(map(lambda x: x * number, my_list[:]))
