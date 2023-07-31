3560a7823a9a7c79841157742636bf841988a967
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-07-31 08:53:23 -0700

    task

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,5 +4,7 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except Exception as e:
+        import sys
         sys.stderr.write("Exception: {}".format(str(e)))
+        sys.stderr.flush()
         return False
