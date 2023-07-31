d4be0b385567a1be1d7e53bfc31da83d570c1cd7
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-07-31 05:21:28 -0700

    task

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    y = 0
+    for i in range(0, x):
+        try:
+            print(m"{:d}".format(y_list[i]), end='')
+            y += 1
+        except (IndexError, ValueError, TypeError):
+            print("")
+            return y
+    print("")
+    return y
