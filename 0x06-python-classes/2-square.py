95d2211a4954d23ee3af2a38f12946b0ab4dd462
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-08-01 08:44:05 -0700

    task

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""A module that contains the definition for an empty class
+"""
+
+
+class Square:
+    """A class the defines a square.
+
+    Args:
+        size (noval): The size of the square.
+
+    Attributes:
+        __size: Private attribute size.
+    """
+    def __init__(self, size=0):
+        """Constructor function.
+
+        Note: raise exceptions if size is not int.
+
+        Args:
+            size (int): The size from the user.
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
