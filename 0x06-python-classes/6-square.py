c2ac77ff9384bf1aa9a8186ed60e591d14eeea42
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-08-01 10:00:50 -0700

    task

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -80,13 +80,13 @@ class Square:
         Raises:
             TypeError: if not int.
         """
-        if type(position) != tuple:
+        if type(value) != tuple:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if type(position[0]) != int or type(position[1]) != int:
+        if type(value[0]) != int or type(value[1]) != int:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if position[0] < 0 or position[1] < 0:
+        if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = postition
+        self.__position = value
 
     def my_print(self):
         """prints the square.
