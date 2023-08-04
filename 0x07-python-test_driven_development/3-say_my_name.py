3966fd3754f46a6cd87ae4a8c1d19aa63bc9fe2f
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-08-04 11:27:11 -0700

    task

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""A module for a function that prints the name.
+
+The function takes two args:
+    first name
+    second name
+
+Both names should be strings.
+"""
+
+def say_my_name(first_name, last_name=""):
+    """A function that prints first and last name.
+
+    Args:
+        first_name(str): first name
+        last_name(str): optional last name
+    """
+    if type(first_name) != str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) != str:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This is a module that contains print_square
+
+Print_square is a fuction that prints a square and takes a size.
+The size must be an int or else raise TypeError.
+"""
+
+def print_square(size):
+    """A function that prints a square.
+
+    Args:
+        size(int): size of the square to be printed.
+    """
+    if type(size) != int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        for j in range(size):
+            print("#", end='')
+        print("")
