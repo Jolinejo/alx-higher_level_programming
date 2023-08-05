75c1e5cce93ca8d4a87fa518ee945d7a46ada54f
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-08-05 10:51:01 -0700

    task

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""This module contains a function for indentation
+
+The function takes a string and splits it
+
+splitting happens at : . and ?
+"""
+def text_indentation(text):
+    """A function for printing the string splitted.
+
+    Args:
+        text(str): the string to print
+
+    Notes:
+        Raises a TypeError if text not str.
+    """
+    if type(text) != str:
+        raise TypeError("text must be a string")
+    line = ""
+    for i in range(len(text)):
+        if text[i] in ".?:":
+            line += text[i]
+            for i in range(len(line)):
+                if line[i] != " ":
+                    print("{}\n".format(line[i:]))
+                    line = ""
+                    break
+        else:
+            line += text[i]
+    if line != "":
+        start = None
+        _end = None
+        for i in range(len(line)):
+            if line[i] != " ":
+                start = i
+                break
+        for i in range(len(line), 0, -1):
+            if line[i-1] != " ":
+                _end = i
+                break
+        if start != None:
+            print("{}".format(line[start: _end]), end='')
---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInt(unittest.TestCase):
+    """A class for unit testing the max_integer
+    
+    Is a subclass of the unittest TestCase
+    """
+    def test_max(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([4, 3, 2, 1]), 4)
+        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
+        self.assertEqual(max_integer([]), None)
+        self.assertEqual(max_integer([1, 1, 1]), 1)
+        self.assertEqual(max_integer([-2, -1, -3]), -1)
+        self.assertEqual(max_integer([1]), 1)
