6e18839020fc8d1f50d112e749d1bd7da0e04192
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-08-08 04:39:38 -0700

    task

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A module that contains a locked class.
+
+"""
+
+
+class LockedClass():
+    """A locked Class.
+
+    Attributes:
+        first_name: only attr
+    """
+     __slots__ = ['first_name']
