33264eaaa5668d01f089d05d3bb74840aa08544f
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-08-09 06:02:15 -0700

    task

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -20,8 +20,12 @@ def add_integer(a, b=98):
     Returns:
         Sum of the two nums.
     """
-    if a is None or type(a) not in [int, float] or a != a:
+    if a is None or type(a) not in [int, float]:
         raise TypeError("a must be an integer")
-    if type(b) not in [int, float] or b != b:
+    if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
+    if a != a:
+        a = 5
+    if b != b:
+        b = 5
     return int(a) + int(b)
