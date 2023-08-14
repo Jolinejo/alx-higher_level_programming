402ae0ddcaa2d4bce6028a6fbdad514439e25087
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-08-14 09:51:31 -0700

    Task

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -7,8 +7,8 @@ class MyInt(int):
     """Rebel int class"""
     def __eq__(self, other):
         """equal operator."""
-        return self != other
+        return int(self) != int(other)
 
     def __ne__(self, other):
         """not equal operator."""
-        return self == other
+        return int(self) == int(other)
