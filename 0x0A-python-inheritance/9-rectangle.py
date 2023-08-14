dfef9024f755ce444e9e56c193f02b98b3d2b907
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-08-14 09:04:38 -0700

    Task

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""A module that contains the definition for an empty class
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """A class the defines a rectangle.
+
+    Args:
+        width(int): The width of the square.
+        height(int): The height of the square.
+
+    Attributes:
+        __width: Private attribute width.
+        __height: Private attribute height.
+    """
+    def __init__(self, width, height):
+        """Constructor function.
+
+        Note: raise exceptions if size is not int.
+
+        Args:
+            width (int): The width from the user.
+            height (int): The height from the user.
+        """
+        self.integer_validator("width", width)
+        self.__wdith = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """return rec area"""
+        ar = self.__width * self.__height
+        return ar
+
+    def __str__(self):
+        """str represintation"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
