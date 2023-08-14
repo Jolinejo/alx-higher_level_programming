2ed7aed66013c3618c58bf0e0867444fe75d4bd7
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-08-14 09:40:40 -0700

    Task

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""A module that contains the definition for an empty class
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A class the defines a square.
+
+    Args:
+        size (noval): The size of the square.
+
+    Attributes:
+        __size: Private attribute size.
+    """
+    def __init__(self, size):
+        """Constructor function
+
+        Args:
+            size (noval): The size from the user.
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        """str represintation"""
+        s = "[Square] "
+        s += str(self.__size) + "/" + str(self.__size)
+        return s
