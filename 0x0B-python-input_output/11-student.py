5c8f584db1483ad58cb5e4b197f2e9210e15fc18
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-08-15 15:01:14 -0700

    task

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -36,4 +36,5 @@ class Student:
         return self.__dict__
 
     def reload_from_json(self, json):
-        self.__dict__ = json
+        for key, val in json.items():
+            setattr(self, key, val)
