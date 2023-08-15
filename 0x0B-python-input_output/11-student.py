a0dd793d6837a3ba4813d4148e42496f1312f5c6
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-08-15 14:58:00 -0700

    task

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""A module that contains the definition for an empty class
+"""
+
+
+class Student:
+    """A class the defines a student.
+
+    Attributes:
+        first_name(str): public
+        last_name(str): public
+        age(int): public
+    """
+    def __init__(self, first_name, last_name, age):
+        """constructir func"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is not None:
+            if type(attrs) == list:
+                flag = 0
+                for i in attrs:
+                    if type(i) != str:
+                        flag = 1
+                        break
+                if flag == 0:
+                    dct = {}
+                    for i in attrs:
+                        try:
+                            dct[i] = self.__dict__[i]
+                        except Exception:
+                            pass
+                    return dct
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        self.__dict__ = json
