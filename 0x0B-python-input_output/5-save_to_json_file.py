f48bea36ce10d405180c6678e940dcb0d2e73083
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-08-15 12:21:12 -0700

    task

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -10,6 +10,5 @@ def save_to_json_file(my_obj, filename):
     Args:
         filename: file
     """
-    data = json.loads(my_obj)
     with open(filename, 'w+') as f:
-        json.dump(data, f)
+        json.dump(my_obj, f)
