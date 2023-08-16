afa0006050744ab92144ac8c3cbeb4a32a1afee8
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-08-16 08:35:20 -0700

    task

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
-
+"""
+100-append_after.py
+"""
 
 
 def append_after(filename="", search_string="", new_string=""):
+    """func to append text
+
+    Args:
+        filename
+        search_string
+        new_string
+    """
     s = ""
     with open(filename, encoding="utf-8") as myfile:
         for line in myfile:
