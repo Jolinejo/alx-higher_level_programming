0788189d745febab7fb5c0aafd2a4603f62168c0
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-08-19 11:04:15 -0700

    task

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -19,3 +19,13 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """size getter"""
+        return self.width
+
+    @size.setter
+    def size(self, val):
+        """size setter"""
+        self.width = val
+        self.height = val
