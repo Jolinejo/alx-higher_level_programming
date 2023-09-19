a299e1e0187f30b3c883bf820c0c4e8c261c2c71
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-09-19 07:33:19 -0700

    Task 15

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 -- similar scores
-SELECT score, COUNT(*) AS "number" 
-FROM second_table 
+SELECT score, COUNT(*) AS number
+FROM second_table
 GROUP BY score
-ORDER BY "number" DESC
+ORDER BY number DESC;
