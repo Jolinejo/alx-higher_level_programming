25c78dbaca27a4ae3d75a9aaf62ff38898de268e
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-09-19 08:01:27 -0700

    moving

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,7 +2,7 @@
 -- alter database
 ALTER DATABASE
     hbtn_0c_0
-    CHARACTER SET = utf8mb4
+    DEFAULT CHARACTER SET = utf8mb4
     COLLATE = utf8mb4_unicode_ci;
 -- alter table
 USE hbtn_0c_0;
@@ -14,7 +14,7 @@ ALTER TABLE
 USE hbtn_0c_0;
 ALTER TABLE
     first_table
-    CHANGE name name
+    MODIFY name
     VARCHAR(256)
     CHARACTER SET utf8mb4
     COLLATE utf8mb4_unicode_ci;
