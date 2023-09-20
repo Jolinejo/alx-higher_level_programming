356e59220d9345b9eed3330e0a029440772e9c30
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-09-20 09:56:57 -0700

    test

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,8 +1,6 @@
 -- rating
 SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
-FROM tv_shows INNER JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN tv_show_ratings
-ON tv_show_ratings.show_id = tv_show_genres.show_id
+FROM tv_shows INNER JOIN tv_show_ratings
+ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY tv_shows.title
 ORDER BY rating DESC;
