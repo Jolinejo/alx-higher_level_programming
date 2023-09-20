9c47f172857c706789938587573263a0e92e04af
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-09-20 06:29:05 -0700

    create

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,4 +2,4 @@
 -- create
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 -- SET PRIVILAGES
-GRANT * ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
