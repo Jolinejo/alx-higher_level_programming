bff1bba088a26d727db001b33af371b01ebaa21b
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-09-20 06:34:40 -0700

    creat db and user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,6 +2,6 @@
 -- create db
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- create user
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd'
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 -- grant privilages 
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
