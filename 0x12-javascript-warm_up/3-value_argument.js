b7b6143915ab517fe0af949b7d331f26570c660d
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-10-16 12:57:57 -0700

    Task

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,9 +2,9 @@
 
 const args = process.argv;
 let fl = false;
-for (let i = 2; args[i]; i++) {
-    console.log(args[i]);
-    fl = true;
+for (let i = 2; i < 3; i++) {
+  console.log(args[i]);
+  fl = true;
 }
 if (!fl) {
   console.log('No argument');
