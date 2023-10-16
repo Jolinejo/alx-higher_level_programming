d17007f833450a11b908016a911a1ec2e2eabe3d
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-10-16 12:26:00 -0700

    task

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const toPrint1 = 'C is fun';
-const toPrint2 = 'Python id cool';
+const toPrint2 = 'Python is cool';
 const toPrint3 = 'JavaScript is amazing';
 console.log(toPrint1);
 console.log(toPrint2);
