5cb60e98dea8a46408bea01dac30a49ea0129f33
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-10-16 13:11:42 -0700

    task

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,5 +2,9 @@
 
 const args = process.argv;
 const c1 = args[2] ? args[2] : 'Not a number';
-const c2 = parseInt(c1, 10) ? parseInt(c1, 10) : 'Not a number';
-console.log(c2);
+const c2 = parseInt(c1, 10);
+if (c2) {
+  console.log('My number: ' + c2);
+} else {
+  console.log(c2);
+}
