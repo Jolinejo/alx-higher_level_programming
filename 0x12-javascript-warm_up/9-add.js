2c8aa101a55c60f614d01492f7ee162cb027948b
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-10-17 04:16:23 -0700

    task

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+function add(a, b) {
+  return (a + b)
+}
+const args = process.argv;
+const c1 = args[2] ? args[2] : 'NaN';
+const c3 = args[3] ? args[3] : 'NaN';
+const c2 = parseInt(c1, 10);
+const c4 = parseInt(c3, 10);
+if (!c2) {
+  console.log('NaN');
+} else if (!c4) {
+  console.log('NaN');
+} else {
+  console.log(add(c2, c4));
+}
