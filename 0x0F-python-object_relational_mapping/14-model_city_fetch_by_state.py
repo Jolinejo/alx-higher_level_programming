a3778e6f40e339f3f3c1f14debfc85a6985ee67e
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-10-20 16:27:22 -0700

    task

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+from model_city import City
+from sqlalchemy.orm import Session
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    name = sys.argv[1]
+    pwd = sys.argv[2]
+    db = sys.argv[3]
+    host = "localhost"
+    port = 3306
+    query = 'mysql+mysqldb://{}:{}@{}:{}/{}'.format(name, pwd, host, port, db)
+    engine = create_engine(query, pool_pre_ping=True)
+    session = Session(engine)
+    for c, s in session.query(City, State) \
+            .filter(City.state_id == State.id).all():
+        print("{}: ({}) {}".format(s.name, c.id, c.name))
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Class in database
+"""
+from model_state import Base, State
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class City(Base):
+    """
+    City class
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128))
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
