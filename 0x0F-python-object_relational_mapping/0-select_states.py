e128fb5a7b7b6c4ef5d8cc259ddfffbdf4571bcf
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-10-20 05:54:27 -0700

    task

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+module for printing states
+"""
+def main():
+    """
+    main states func
+    """
+    import MySQLdb
+    import sys
+    name = sys.argv[1]
+    pwd = sys.argv[2]
+    db = sys.argv[3]
+    host = "localhost"
+    port = 3306
+    db_connection = MySQLdb.connect(host=host, user=name, port=port,
+                                    password=pwd, database=db)
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT * FROM states;")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+
+if __name__ == "__main__":
+    main()
