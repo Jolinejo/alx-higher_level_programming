63ff209b2820484169f4fad0093bf60c3afdd876
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-10-20 17:01:09 -0700

    task

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -2,8 +2,8 @@
 """Start link class to table in database
 """
 import sys
-from model_state import Base, State
-from model_city import City
+from relationship_state import Base, State
+from relationship_city import City
 from sqlalchemy.orm import Session
 from sqlalchemy import (create_engine)
 
@@ -20,4 +20,5 @@ if __name__ == "__main__":
     newS = State(name="California")
     newC = City(name="San Francisco")
     newS.cities.append(newC)
+    session.add(newS)
     session.commit()
