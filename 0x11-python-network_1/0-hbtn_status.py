deac22392e51c17b4233dd3c87839e8ddb7ed6b8
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-11-03 12:32:50 -0700

    task

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module to fetch URL"""
+
+import urllib.request
+with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+   html = response.read()
+   print("Body response:")
+   print ("    - type: {}".format(type(html)))
+   print("    - content: {}".format(html))
+   print("    - utf8 content: {}".format(html.decode('utf-8')))
