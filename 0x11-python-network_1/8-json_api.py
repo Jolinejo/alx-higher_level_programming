d617efb33355594840489e6babe51d4f70ed2b9c
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-11-05 22:48:09 -0800

    task

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""module to fetch URL"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+    data = {}
+    try:
+        data['q'] = sys.argv[1]
+    except Exception:
+        data['q'] = ""
+    r = requests.post('http://0.0.0.0:5000/search_user', data)
+    try:
+        dictti = r.json()
+        if len(dictti) != 0:
+            print("[{}]: {}".format(dictti["id"], dictti["name"]))
+        else:
+            print("No result")
+    except requests.exceptions.JSONDecodeError:
+        print("Not a valid JSON")
+
