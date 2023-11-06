6366660476de21224b6708de2539085e5ccb6f57
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-11-06 06:11:02 -0800

    task

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     from requests.auth import HTTPBasicAuth
     url = "https://api.github.com/user"
     r = requests.get(url, auth=HTTPBasicAuth(sys.argv[1], sys.argv[2]))
-    print(r.json()["id"])
+    print(r.json().get("id"))
