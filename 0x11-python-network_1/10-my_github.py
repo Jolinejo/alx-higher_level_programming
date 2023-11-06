8bd5cf0cb980d900ddbac023e73e80eae071d17c
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-11-06 06:09:42 -0800

    task

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module to fetch URL"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+    from requests.auth import HTTPBasicAuth
+    url = "https://api.github.com/user"
+    r = requests.get(url, auth=HTTPBasicAuth(sys.argv[1], sys.argv[2]))
+    print(r.json()["id"])
